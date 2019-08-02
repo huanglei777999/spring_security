9b5d1da8d10a84835b602eb35d713da48df2764b
huanglei777999 <18003769477@163.com>
2019-08-02 15:54:07 +0800

    第二次修改

--- springsecurity/src/test/java/com/hl/springsecurity/SpringsecurityApplicationTests.java
+++ springsecurity/src/test/java/com/hl/springsecurity/SpringsecurityApplicationTests.java
@@ -1,7 +1,10 @@
 package com.hl.springsecurity;
 
+import com.hl.springsecurity.dao.UserMapper;
+import com.hl.springsecurity.model.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringsecurityApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     public void contextLoads() {
+        User admin = userMapper.getUserFromDatabase("admin");
+        System.out.println(admin);
     }
 
+    @Test
+    public void test1(){
+        System.out.println("hello");
+    }
 }
